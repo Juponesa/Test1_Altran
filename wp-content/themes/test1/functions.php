7977dc6ec32d06a8e55f4febb1c8a43917a648f3
Juliana Sayuri <julianaotsuka@gmail.com>
2016-07-04 22:12:08 -0300

    Create functions with includes

--- wp-content/themes/test1/functions.php
+++ wp-content/themes/test1/functions.php
@@ -1,22 +1,6 @@
 <?php
 
-function _import()
-{
-    _register_style('open-sans', 'http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800');
-    _register_style('style', css_url() . '/style.css');
-}
-
-add_action('wp_enqueue_scripts', '_import', 10);
-
-function _register_style($handle, $src, $depts = array(), $ver = null, $media = false)
-{
-    wp_dequeue_style($handle);
-    wp_deregister_style($handle);
-    wp_register_style($handle, $src, $depts, $ver, $media);
-    wp_enqueue_style($handle);
-}
-
-function css_url()
-{
-    return get_template_directory_uri() . '/css';
-}
+include "functions/assets.php";
+include "functions/post-types.php";
+include "functions/custom-meta.php";
+include "functions/Post_Data.php";
