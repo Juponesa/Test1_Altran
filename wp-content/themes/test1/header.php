db393f7007c22b1ce0ac1ea31a16beff4a20793b
Juliana Sayuri <julianaotsuka@gmail.com>
2016-07-04 22:06:56 -0300

    Add menu

--- wp-content/themes/test1/header.php
+++ wp-content/themes/test1/header.php
@@ -3,8 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Teste 1</title>
+    <title>Teste 1 - Post Types</title>
 
     <?php wp_head(); ?>
 </head>
 <body>
+
+<header>
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+                        aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="<?php home_url() ?>">Controle de Estoque</a>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                    <li><a href="<?php home_url() ?>/produtos">Produtos</a></li>
+                    <li><a href="<?php home_url() ?>/clientes">Clientes</a></li>
+                    <li><a href="<?php home_url() ?>/pedidos">Pedidos</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
